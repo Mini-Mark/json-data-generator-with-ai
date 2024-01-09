3297711c934742fb0728e4cd59ac01660501274d
Phongphiphat Senta <MiniMarkChanel @gmail.com>
2024-01-09 11:18:17 +0700

    🚧 Update: - re json format - Add Config file

--- json_data_generator.py
+++ json_data_generator.py
@@ -2,9 +2,11 @@ import google.generativeai as genai
 import tkinter as tk
 from tkinter import ttk
 import threading
+import config
 
-genai.configure(api_key="") #Set your API key here
+genai.configure(api_key= config.GOOGLE_API_KEY )
 model = genai.GenerativeModel('gemini-pro')
+
 
 def convert_data():
     submit_button.config(state="disabled")
@@ -15,9 +17,10 @@ def convert_data():
     result_text.config(state="disabled")
     threading.Thread(target=getAnswer).start()
 
+
 def getAnswer():
     json_data = json_text.get("1.0", "end-1c")
-    result_data = f"Generate more list of json data with this json:\n{json_data}\n\nPlease generate with the same key and random data from your idea. Give me only JSON. Don't tell anything without JSON"
+    result_data = f"Generate more list of json data with this json:\n{json_data}\n\nPlease generate with the same key and random data from your idea. if data have many than one make it to list []. Give me only JSON. Don't tell anything without JSON"
 
     response = model.generate_content(result_data, stream=True)
 
@@ -27,8 +30,9 @@ def getAnswer():
         ai_result += chunk.text
         ai_result += "\n"
 
-    format_result = ai_result.replace("```json","").replace("\n```","").replace("```","")
-    
+    format_result = ("\n".join(ai_result.replace(
+        "```json", "").split("```"))).replace("\n\n", "\n").strip()
+
     result_text.config(state="normal")
     result_text.delete("1.0", "end")
     result_text.insert("1.0", format_result)
@@ -36,18 +40,21 @@ def getAnswer():
     submit_button.config(state="normal")
     copy_button.config(state="normal")
 
+
 def copy_to_clipboard():
     result_text_content = result_text.get("1.0", "end-1c")
     root.clipboard_clear()
     root.clipboard_append(result_text_content)
     root.update()
 
+
 # Create the main window
 root = tk.Tk()
 root.title("Json Data Generator")
 
 # Create and place widgets
-json_label = tk.Label(root, text="JSON Data (Ex. Pokemon, {json text}, Person) :")
+json_label = tk.Label(
+    root, text="JSON Data (Ex. Pokemon, {json text}, Person) :")
 json_label.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 json_text = tk.Text(root, height=30, width=60)
 json_text.grid(row=1, column=0, padx=10, pady=5)
